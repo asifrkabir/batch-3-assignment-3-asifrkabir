2a7238a4218c668b8871c181cdb4c454bf352ce3
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-06-13 19:46:28 +0600

    createUserValidationSchema created

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { USER_ROLE_LIST } from "./user.constant";
+
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a valid string",
+    }),
+    email: z
+      .string({ required_error: "Email is required" })
+      .email({ message: "Invalid email address" }),
+    password: z.string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a valid string",
+    }),
+    phone: z.string({
+      required_error: "Phone number is required",
+      invalid_type_error: "Phone number must be a valid string",
+    }),
+    address: z.string({
+      required_error: "Address is required",
+      invalid_type_error: "Address must be a valid string",
+    }),
+    role: z.enum([...USER_ROLE_LIST] as [string, ...string[]], {
+      required_error: "Role is required",
+    }),
+  }),
+});
+
+export const UserValidations = {
+  createUserValidationSchema,
+};
