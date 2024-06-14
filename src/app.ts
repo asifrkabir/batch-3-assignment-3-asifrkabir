77dfbb5610f3b404c699316dd3f1247517e16e5d
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-06-14 12:44:45 +0600

    errors handled

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
 
@@ -15,5 +17,9 @@ app.use("/api", router);
 app.get("/", (req: Request, res: Response) => {
   res.send("Bike Rental Reservation System is listening on port 5000");
 });
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
